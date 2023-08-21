9f40e601d8c123ea583af3c4df5de36f86c7f517
Katielly <katiellyscherbatsky@gmail.com>
2023-08-21 19:48:39 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const somar = require("./somar");
+
+let resultado = somar(1, 3);
+console.log("O resultado da soma = " + resultado);
+
+const reajusteSalarial = require("./exec01");
+
+reajusteSalarial("João", 1500);
